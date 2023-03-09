618ac792d54bff170bfc86c3d61e644a6d5bebbb
Lynix25 <pauleric22524@gmail.com>
2023-03-09 14:03:07 +0700

    Repair Null Data While Create Room

--- js/library.js
+++ js/library.js
@@ -44,10 +44,8 @@ mainJs.setAttribute("src", "js/main.js");
 document.head.appendChild(mainJs);
 
 // DEV NavBar Component
-let filesystem = "D:/IndekostManager-FE"
 let currentPath = window.location.pathname;
-if (currentPath !== `/${filesystem}/login.html` && 
-    currentPath !== `/${filesystem}/forgotpassword.html`)
+if (currentPath !== `/login.html` && currentPath !== `/forgotpassword.html`)
 document.addEventListener('DOMContentLoaded', e => {
     let navBar = document.createElement("nav");
     navBar.classList.add("navbar", "navbar-expand-lg", "sticky-top");
