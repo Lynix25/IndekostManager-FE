b08552d532512c1217cfb1d8d4557737a3c2c629
Meiriska Amelia <kim07.my231@gmail.com>
2023-03-28 09:56:06 +0700

    Enhance login

--- js/login.js
+++ js/login.js
@@ -1,11 +1,11 @@
-import { END_POINT } from "./config.js"
-import { APIPost, getFormValue, goTo, handleFormSubmited } from "./utils.js";
+import { Toast } from "./component/toast.js";
+import { Constant, ServiceURL } from "./config.js";
+import { APIPost, getFormValue, goTo, handleFormSubmited, setCookie } from "./utils.js";
 
 window.addEventListener('load', e => {
     document.querySelector("#password-show-hide").addEventListener("click", e => {
         passwordShowHide("#password", ".fa-eye", ".fa-eye-slash")
     })
-
     handleFormSubmited(login)
 })
 
@@ -27,15 +27,18 @@ function passwordShowHide(inputTarget, showIcon, hideIcon) {
 
 function login(e) {
     let data = getFormValue(e.target);
-    APIPost("/account/login", data).then(response => {
+    APIPost(ServiceURL.User.login, data).then(response => {
         if (response.status == 200) {
-            document.cookie = "tokens="+response.data.data.user.id;
-            goTo('./home.html');
+            let value = response.data.data.token.tokenId + "|role=" + response.data.data.user.role.name;
+            setCookie("tokens", value, response.data.data.token.expiresIn);
+            Toast(Constant.httpStatus.SUCCESS, response.data.message);
+            setTimeout(function() { goTo('./home.html') }, 300);
         }
-    }).catch(response => {
-        console.log(response)
+    }).catch(err => {
+        let message = "Unknown Error";
+        if((err.data.message).toLowerCase() === "invalid username or password") message = err.data.message;
+        else message = "User not registered";
+
+        Toast(Constant.httpStatus.ERROR, message);
     })
 }
-
-
-
