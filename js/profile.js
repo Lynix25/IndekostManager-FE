b349c66ddcd8397ac3ca88f788d5b9f8cbc3f874
Meiriska Amelia <kim07.my231@gmail.com>
2023-04-25 23:47:43 +0700

    Add edit user (tenant) and room detail

--- js/profile.js
+++ js/profile.js
@@ -1,14 +1,15 @@
-import { APIDelete, APIGet, APIPost } from "./api.js";
+import { APIDelete, APIGet } from "./api.js";
 import { showModalConfirmation } from "./component/modal.js";
 import { Toast } from "./component/toast.js";
 import { Constant, Event, ServiceURL } from "./config.js";
 import { getCookie } from "./cookiemanagement.js";
 import { showModalForm } from "./createcontactable.js";
 import { logout } from "./main.js";
-import { addCustomEventListener, getFormValue, goTo, handleFormSubmited, UNIXtimeConverter } from "./utils.js";
+import { addCustomEventListener, goTo, UNIXtimeConverter } from "./utils.js";
 
 APIGet(ServiceURL.User.getById(getCookie('id'))).then(res => {
     let user = res.data.data.user;
+    let room = res.data.data.room;
     
     document.querySelector("#name").innerText = user.name;
     document.querySelector("#alias").innerText = `(${user.alias})`;
@@ -106,6 +107,8 @@ APIGet(ServiceURL.User.getById(getCookie('id'))).then(res => {
             });
         }
     });
+
+    getRoomData(room.id)
 });
 
 addCustomEventListener("show-room-info", e => {
@@ -124,6 +127,118 @@ addCustomEventListener("add-alternatif-contact", e => {
 
 let eLogout = document.getElementById("logout");
 logout(eLogout);
+
+function getRoomData(roomId) {
+    APIGet(ServiceURL.Room.getById(roomId)).then(res => {
+        let room = res.data.data;
+        document.querySelector(".name").innerHTML = room.room.name;
+        document.querySelector(".allotment").innerHTML = room.room.allotment;
+        document.querySelector(".floor").innerHTML = 'Lantai ' + room.room.floor;
+        document.querySelector(".description").innerHTML = room.room.description;
+
+        let status = (room.totalTenants >= room.room.quota ? 'Kamar penuh' : 'Tersedia');
+        document.querySelector(".status").classList.add(status === 'Kamar penuh' ? "text-danger" : "text-success");
+        status += ` (${room.totalTenants}/${room.room.quota})`;
+        document.querySelector(".status").innerHTML = status;
+
+        // Room Details
+        let details = room.room.details;
+        details.forEach(detail => {
+            let icon = detail.enable ? "fa-check-circle" : "fa-times-circle";
+            if(detail.masterRoomDetailCategory.name === Constant.roomDetailsCategory.KAMAR_TIDUR) {
+                let itemList = document.createElement("div");
+                itemList.classList.add("d-flex", "col", "align-items-center")
+
+                itemList.innerHTML = `
+                    <div class="w-100">
+                        <div class="d-flex align-items-center">
+                            <li class="w-100">${detail.name}</li>
+                            <i class="fa ${icon}"></i>
+                        </div>
+                        <div class="font-italic">This is description</div>
+                    </div>
+                `;
+                document.querySelector("#kamar-tidur").appendChild(itemList);
+            }
+            else if(detail.masterRoomDetailCategory.name === Constant.roomDetailsCategory.KAMAR_MANDI) {
+                let itemList = document.createElement("div");
+                itemList.classList.add("d-flex", "col", "align-items-center")
+
+                itemList.innerHTML = `
+                    <div class="w-100">
+                        <div class="d-flex align-items-center">
+                            <li class="w-100">${detail.name}</li>
+                            <i class="fa ${icon}"></i>
+                        </div>
+                        <div class="font-italic">This is description</div>
+                    </div>
+                `;
+                document.querySelector("#kamar-mandi").appendChild(itemList);
+            }
+            else if(detail.masterRoomDetailCategory.name === Constant.roomDetailsCategory.FURNITURE) {
+                let itemList = document.createElement("div");
+                itemList.classList.add("d-flex", "col", "align-items-center")
+                
+                itemList.innerHTML = `
+                    <div class="w-100">
+                        <div class="d-flex align-items-center">
+                            <li class="w-100">${detail.name}</li>
+                            <i class="fa ${icon}"></i>
+                        </div>
+                        <div class="font-italic">This is description</div>
+                    </div>
+                `;
+                document.querySelector("#furniture").appendChild(itemList);
+            }
+            else if(detail.masterRoomDetailCategory.name === Constant.roomDetailsCategory.ALAT_ELEKTRONIK) {
+                let itemList = document.createElement("div");
+                itemList.classList.add("d-flex", "col", "align-items-center")
+                
+                itemList.innerHTML = `
+                    <div class="w-100">
+                        <div class="d-flex align-items-center">
+                            <li class="w-100">${detail.name}</li>
+                            <i class="fa ${icon}"></i>
+                        </div>
+                        <div class="font-italic">This is description</div>
+                    </div>
+                `;
+                document.querySelector("#alat-elektronik").appendChild(itemList);
+            }
+            else {
+                let itemList = document.createElement("div");
+                itemList.classList.add("d-flex", "col", "align-items-center")
+                
+                itemList.innerHTML = `
+                    <div class="w-100">
+                        <div class="d-flex align-items-center">
+                            <li class="w-100">${detail.name}</li>
+                            <i class="fa ${icon}"></i>
+                        </div>
+                        <div class="font-italic">This is description</div>
+                    </div>
+                `;
+                document.querySelector("#fasilitas-lain").appendChild(itemList);
+            }
+        });
+
+        let prices = room.room.prices;
+        prices.forEach(price => {
+            let item = document.createElement("tr");
+            item.innerHTML = `
+                <td>${price.capacity} orang</td>
+                <td>Rp ${price.price},-</td>
+            `
+            document.querySelector("#price-data").appendChild(item);
+        });
+
+        document.querySelector("#category1").innerHTML = Constant.roomDetailsCategory.KAMAR_TIDUR;
+        document.querySelector("#category2").innerHTML = Constant.roomDetailsCategory.KAMAR_MANDI;
+        document.querySelector("#category3").innerHTML = Constant.roomDetailsCategory.FURNITURE;
+        document.querySelector("#category4").innerHTML = Constant.roomDetailsCategory.ALAT_ELEKTRONIK;
+        document.querySelector("#category5").innerHTML = Constant.roomDetailsCategory.FASILITAS_KAMAR_LAINNYA;
+    });
+}
 
 let js = document.createElement("script");
 js.innerHTML = `
